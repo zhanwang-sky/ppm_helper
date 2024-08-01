74eea222f184981998e33c38a7d60cb5d1dfd070
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2024-08-01 21:22:16 +0800

    Initial Commit

---
+++ ppm_helper/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ppm_helper
+//
+//  Created by zhanwang-sky on 2024/8/1.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
